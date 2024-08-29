ecae903af7eef469a389c8e98d3cd3dbdcc56655
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-08-29 11:23:20 +0200

    exercice03

--- exercices/exercice02/Exercice02.java
+++ exercices/exercice02/Exercice02.java
@@ -3,7 +3,7 @@ package exercices.exercice02;
 public class Exercice02 {
     public static void main(String[] args) {
         System.out.println("bonjour tout le monde");
-    
-}
-    
+
+    }
+
 }
---
+++ exercices/exercice03/TypeDeBase.java
@@ -0,0 +1,13 @@
+package exercices.exercice03;
+
+public class TypeDeBase {
+    public static void main(String[] args) {
+        byte age = 15;
+        byte age10 = 25;
+        String nom = "Matias";
+        System.out.println("Je m'appelle " + nom + " et j'ai " + age + "ans.");
+        System.out.println("Dans 10ans, j'aurai " + age10 + " ans.");
+
+    }
+
+}
