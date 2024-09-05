3ddb90a6511cf07617edb2afd9f1abd37f98b3b9
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-05 08:35:27 +0200

    exercice3

--- exercices/exercice03/TypeDeBase.java
+++ exercices/exercice03/TypeDeBase.java
@@ -3,7 +3,7 @@ package exercices.exercice03;
 public class TypeDeBase {
     public static void main(String[] args) {
         byte age = 15;
-        int age10 = age+10;
+        int age10 = age + 10;
         String nom = "Matias";
         System.out.println("Je m'appelle " + nom + " et j'ai " + age + "ans.");
         System.out.println("Dans 10ans, j'aurai " + age10 + " ans.");
---
+++ exercices/exercice04/EchangeValeurs.java
@@ -0,0 +1,18 @@
+package exercices.exercice04;
+
+public class EchangeValeurs {
+    public static void main(String[] args) {
+        int variable1 = 10;
+        int variable2 = 9;
+        System.out.println("Le contenu de la variable1 est " + variable1);
+        System.out.println("Le contenu de la variable2 est " + variable2);
+        System.out.println("...traitement...");
+        int variable3=variable1;
+        variable1 = variable2;
+        variable2 = variable3;
+        System.out.println("Le contenu de la variable1 est " + variable1);
+        System.out.println("Le contenu de la variable2 est " + variable2);
+
+    }
+
+}
