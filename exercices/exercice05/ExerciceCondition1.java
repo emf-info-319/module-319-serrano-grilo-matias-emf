8467d3478cc67550de4560601e6a28ff28d46e4e
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-05 08:55:28 +0200

    exercice5condition1

---
+++ exercices/exercice05/ExerciceCondition1.java
@@ -0,0 +1,15 @@
+package exercices.exercice05;
+
+public class ExerciceCondition1 {
+    public static void main(String[] args) {
+        int valeurATester = 2;
+        if (valeurATester <= 0) {
+            System.out.println("La valeur  est négative");
+
+        } else {
+            System.out.println("La valeur est positive");
+        }
+
+    }
+
+}
