4f1fe0905860fb7ac8f0d4292f82e3aca548344c
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-05 09:12:33 +0200

    exercice5condition2

--- exercices/exercice05/ExerciceCondition2.java
+++ exercices/exercice05/ExerciceCondition2.java
@@ -2,12 +2,13 @@ package exercices.exercice05;
 
 public class ExerciceCondition2 {
     public static void main(String[] args) {
-        int valeurAtester=263;
-        if (valeurAtester%2 <1) {System.out.println("La valeur est paire");    
-        }
-        else{System.out.println("La valeur est impaire");
+        int valeurAtester = 263;
+        if (valeurAtester % 2 < 1) {
+            System.out.println("La valeur est paire");
+        } else {
+            System.out.println("La valeur est impaire");
 
         }
     }
-    
+
 }
