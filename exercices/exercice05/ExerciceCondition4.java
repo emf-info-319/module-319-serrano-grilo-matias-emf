fa4df6db9b0ecb8aadbf950327c88b5d2a032e9d
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-05 10:20:55 +0200

    exercice5condition4

---
+++ exercices/exercice05/ExerciceCondition4.java
@@ -0,0 +1,21 @@
+package exercices.exercice05;
+
+public class ExerciceCondition4 {
+    public static void main(String[] args) {
+        int temperature = 40;
+        if (temperature < -10) {
+            System.out.println("IL fait très froid");
+        } else if (temperature >= -10 && temperature < 0) {
+            System.out.println("Il fait froid");
+        } else if (temperature >= 0 && temperature < 25) {
+            System.out.println("Il fait normal");
+        } else if (temperature >= 25 && temperature <= 35) {
+            System.out.println("Il fait chaud");
+
+        } else {
+            System.out.println("Il fait très chaud");
+        }
+
+    }
+
+}
