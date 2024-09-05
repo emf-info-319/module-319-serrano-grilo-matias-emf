eade5f4ab779b431e454fd20985061a01f66ed1f
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-05 09:47:58 +0200

    exercice5condition3

---
+++ exercices/exercice05/ExerciceCondition3.java
@@ -0,0 +1,17 @@
+package exercices.exercice05;
+
+public class ExerciceCondition3 {
+    public static void main(String[] args) {
+        int valeur1 = 8;
+        int valeur2 = -2;
+        if (valeur1 <=0 && valeur2 <=0){
+            System.out.println("le résultat est positif");
+        } else if (valeur1 >=0 && valeur2 >=0){
+            System.out.println("Le résultat est positif");
+        }else {
+            System.out.println("Le résultat est négatif");
+        }
+        
+    }
+
+}
