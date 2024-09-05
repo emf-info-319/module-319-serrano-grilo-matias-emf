acf29cd36ca70cab3add186e7549550d4055c663
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-05 10:32:35 +0200

    exercice6switch1

---
+++ exercices/exercice06/ExerciceSwitch1.java
@@ -0,0 +1,28 @@
+package exercices.exercice06;
+
+public class ExerciceSwitch1 {
+    public static void main(String[] args) {
+        int noteDuModule = 4;
+        switch (noteDuModule){
+        case 1 :
+        System.out.println("Travail non rendu");
+        break;
+        case 2 :
+        System.out.println("Très insuffisant");
+        break;
+        case 3 :
+        System.out.println("Insufisant");
+        break;
+        case 4 :
+        System.out.println("Suffisant");
+        break;
+        case 5 :
+        System.out.println("Bien");
+        break;
+        case 6 :
+        System.out.println("Très bien");
+        break;
+        }
+    }
+    
+}
