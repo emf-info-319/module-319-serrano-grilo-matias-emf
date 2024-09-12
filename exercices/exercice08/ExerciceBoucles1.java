551adde4f50e23c3fca9aa11f32ba62749d86d24
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-12 09:48:25 +0200

    exercice8boucle1

---
+++ exercices/exercice08/ExerciceBoucles1.java
@@ -0,0 +1,38 @@
+package exercices.exercice08;
+
+public class ExerciceBoucles1 {
+    public static void main(String[] args) {
+        int i = 1;
+        for (i = 1; i <= 5; i++) {
+            System.out.println("Boucle for, itération " + i);
+        }
+        i = 1;
+        System.out.println(" ");
+        while (i <= 5) {
+            System.out.println("Boucle while, itération " + i);
+            i++;
+        }
+        System.out.println(" ");
+        i = 1;
+        do {
+            System.out.println("Boucle do-while, itération " + i);
+            i++;
+        } while (i <= 5);
+        System.out.println(" ");
+        for (i = 1; i <= 5; i++) {
+            if (i == 3) {
+            } else {
+                System.out.println("Boucle for, itération " + i);
+            }
+        }
+        System.out.println(" ");
+        i=1;
+        while (i <= 5) {
+            System.out.println("Boucle while, itération " + i);
+            i++;
+            if (i==4) {
+                break;
+            }
+        }
+    }
+}
