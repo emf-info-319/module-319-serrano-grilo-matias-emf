25d0b247971318f266e62603751380855cd69ab6
serrano-grilo-matias <matias.grilo@gmail.com>
2024-09-12 19:42:32 +0200

    devoir2

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -1,25 +1,28 @@
 package devoirs.devoir02;
 
 public class devoir02 {
-    public final static double CAPACITE_RESERVOIR_A = 3;//variable constante qui ne change jamais
-    public final static double CAPACITE_RESERVOIR_B = 3;
+    public final static double CAPACITE_RESERVOIR_A = 3;// variable constante qui ne change jamais
+    public final static double CAPACITE_RESERVOIR_B = 5;
     public static void main(String[] args) {
-        int remplissage = ( int ) ( Math.random() * ( 8 - 0 + 1 ) ) + 0;//nombre aléatoire
-        System.out.println("Il y a "+remplissage+" litres à remplir.");
-        int reservoirA =0;
-        int reservoirB=0;
-        while(CAPACITE_RESERVOIR_A+CAPACITE_RESERVOIR_B>remplissage){
-            remplissage--;
-            reservoirA++;
-            while (condition) {
-                
-            }
-            if (remplissage==0) {
+        int remplissage = (int) (Math.random() * (8 - 0 + 1)) + 0;// nombre aléatoire entre 0  et 8
+        System.out.println("Il y a " + remplissage + " litres à remplir.");//dit le nombre aléatoire de litre à remplir
+        int reservoirA = 0;//définition de la valeur du résérvoir a
+        int reservoirB = 0;//définition de la valeur du résérvoir b
+        while (CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B >= remplissage) {//tant que la capacité de a et b est plus grand que le remplisage faire ce qui est dedans
+            if (remplissage == 0) {//si le remplissage est égal a 0 arreter
                 break;
             }
-            
+            remplissage--;//remplissage moin 1
+            if (reservoirA < 3) {//si le reservoir a est plus petit que troi faire ça
+                reservoirA++;//reservoir plus 1
+                System.out.println("Remplisage du réservoir A...");
+                System.out.println("Le réservoir A : " + reservoirA + ", le réservoir B : " + reservoirB);//dire combien de litre a chaque reservoir
+            } else {//sinon faire ça
+                reservoirB++;//reservoir b plus 1
+                System.out.println("Remplisage du réservoir B...");
+                System.out.println("Le réservoir A : " + reservoirA + ", le réservoir B : " + reservoirB);//dire combien de litre a chaque reservoir
+            }
         }
-    
+        System.out.println("Remplissage terminé!");
     }
-    
 }
