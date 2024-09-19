3d1fef48d4a641305967381ab79e5cbb9f9a2c7e
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-09-19 08:57:57 +0200

    devoir02

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -4,7 +4,7 @@ public class devoir02 {
     public final static double CAPACITE_RESERVOIR_A = 3;// variable constante qui ne change jamais
     public final static double CAPACITE_RESERVOIR_B = 5;
     public static void main(String[] args) {
-        int remplissage = (int) (Math.random() * (8 - 0 + 1)) + 0;// nombre aléatoire entre 0  et 8
+        int remplissage = (int) (Math.random() * (CAPACITE_RESERVOIR_A+CAPACITE_RESERVOIR_B - 0 + 1)) + 0;// nombre aléatoire entre 0  et 8
         System.out.println("Il y a " + remplissage + " litres à remplir.");//dit le nombre aléatoire de litre à remplir
         int reservoirA = 0;//définition de la valeur du résérvoir a
         int reservoirB = 0;//définition de la valeur du résérvoir b
@@ -13,7 +13,7 @@ public class devoir02 {
                 break;
             }
             remplissage--;//remplissage moin 1
-            if (reservoirA < 3) {//si le reservoir a est plus petit que troi faire ça
+            if (reservoirA < CAPACITE_RESERVOIR_A) {//si le reservoir a est plus petit que troi faire ça
                 reservoirA++;//reservoir plus 1
                 System.out.println("Remplisage du réservoir A...");
                 System.out.println("Le réservoir A : " + reservoirA + ", le réservoir B : " + reservoirB);//dire combien de litre a chaque reservoir
