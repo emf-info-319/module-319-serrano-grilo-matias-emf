275b168a406525641968687492a3cbb84b426da9
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-10-03 11:43:54 +0200

    devoir05

--- devoirs/devoir05/devoir05.java
+++ devoirs/devoir05/devoir05.java
@@ -1,7 +1,10 @@
 package devoirs.devoir05;
 
 public class devoir05 {
+    public final static int MAX=100;
+    public final static int MIN=0;
     public static void main(String[] args) {
+        int nbre = (int)( Math.random() * ( MAX - MIN + 1 )) + MIN;
         
     }
     
