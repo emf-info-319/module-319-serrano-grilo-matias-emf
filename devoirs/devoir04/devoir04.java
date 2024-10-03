cfa5d821c85941e1f448b4b1008b61fabc72fb2a
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-10-03 11:36:35 +0200

    devoir04

--- devoirs/devoir04/devoir04.java
+++ devoirs/devoir04/devoir04.java
@@ -14,13 +14,16 @@ public class devoir04 {
                         System.out.println("date formatée est le " + day + " janvier " + year);
 
                         break;
-                    case 2:if (year % 4 == 0&&day==29) {System.out.println("date formatée est le " + day + " février " + year);
-                        
-                    }else if (day >= 29) {
-                        System.out.println("Le jour [" + day + "] est hors limites !");
+                    case 2:
+                        if (year % 4 == 0 && day == 29) {
+                            System.out.println("date formatée est le " + day + " février " + year);
 
-                    } else {
-                        System.out.println("date formatée est le " + day + " février " + year);}
+                        } else if (day >= 29) {
+                            System.out.println("Le jour [" + day + "] est hors limites !");
+
+                        } else {
+                            System.out.println("date formatée est le " + day + " février " + year);
+                        }
 
                         break;
                     case 3:
---
+++ devoirs/devoir05/devoir05.java
@@ -0,0 +1,8 @@
+package devoirs.devoir05;
+
+public class devoir05 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
