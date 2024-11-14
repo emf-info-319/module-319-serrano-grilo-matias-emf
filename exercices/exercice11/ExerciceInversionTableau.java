fb048312696308fb762a8c5a4a7d0e2cbfa8ee07
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-11-14 08:58:05 +0100

    correction du tab

--- exercices/exercice11/ExerciceInversionTableau.java
+++ exercices/exercice11/ExerciceInversionTableau.java
@@ -9,22 +9,24 @@ public class ExerciceInversionTableau {
         System.out.println("Contenu du tableau initial : ");
         for (int i = 0; i < tableauInitial.length; i++) {
             tableauInitial[i] = (int) (Math.random() * (MAX - MIN + 1) + MIN);
-            System.out.println("Tableau initial ["+i+"] = "+tableauInitial[i]);
+            System.out.println("Tableau initial [" + i + "] = " + tableauInitial[i]);
         }
-        int[]tableauFinal = new int[4];
-        inverseLeTableau(tableauInitial, tableauFinal);
+        int[] tableauFinale = new int[4];
+        tableauFinale = inverseLeTableau(tableauInitial);
         System.out.println("\nContenu du tableau final : ");
-        for (int i = 0; i < tableauFinal.length; i++) {
-            System.out.println("Tableau initial ["+i+"] = "+tableauFinal[i]);
+        for (int i = 0; i < tableauFinale.length; i++) {
+            System.out.println("Tableau initial [" + i + "] = " + tableauFinale[i]);
         }
     }
 
-    public static int[] inverseLeTableau(int[]tableauInitial, int[] tableauFinal) {
-        int fin=tableauInitial.length-1;
-        for (int i = 0; i < tableauFinal.length; i++) {
-            tableauFinal[i]=tableauInitial[fin];
+    public static int[] inverseLeTableau(int[] tableauInitial) {
+        int[] tableauFinal = new int[tableauInitial.length];
+        int fin = tableauInitial.length - 1;
+        for (int i = 0; i < tableauInitial.length; i++) {
+            tableauFinal[i] = tableauInitial[fin];
             fin--;
         }
+
         return tableauFinal;
     }
 
