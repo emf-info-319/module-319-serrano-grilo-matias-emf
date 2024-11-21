3c9fc2b363372c37b65c221c22eb95725cb78253
serrano-grilo-matias-emf <matias.grilo@studentfr.ch>
2024-11-21 08:30:44 +0100

    update

---
+++ revisions/E1_2022_2023/entrainementEval.java
@@ -0,0 +1,8 @@
+package revisions.E1_2022_2023;
+
+public class entrainementEval {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- test.java
+++ test.java
@@ -1,19 +1,17 @@
 
-
 import java.util.Random;
 
 public class test {
 
     public static void main(String[] args) {
-        int[] tab=new int[]{2,6,6,2,1,1,1,1};
-        int nbr = 1;
-        int occurence=0;
-        for (int i = 0; i < tab.length; i++) {
-            if (tab[i] == nbr) {
-                occurence++;
-            }
+        String texte1 = "Bonjour";
+        String texte2 = "Bonjour";
 
+        if (texte1.equals(texte2)) {
+            System.out.println("Les chaînes sont identiques.");
+        } else {
+            System.out.println("Les chaînes ne sont pas identiques.");
         }
-        System.out.println("le nombre "+nbr+" à été trouvée "+occurence+" fois");
+
     }
 }
